2ced2967e50430c43498c1d124ca72baab4f6641
Edelbero David Maestre Nuñez <39956223+edelmaestre@users.noreply.github.com>
2024-08-15 13:06:16 -0500

    Add files via upload

---
+++ application/views/layouts/default.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CodeStack</title>
+</head>
+<body>
+<h1>Encabezado</h1>
+<?php
+	echo $content_for_layout;
+
+	?>
+<h1>Pie de pagina</h1>
+</body>
+</html> 
